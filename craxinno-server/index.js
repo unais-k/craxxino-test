6c78f1c59429cd8e7d03f46a200ce36f73d0a13a
unais-k <unaissafa008@gmail.com>
2023-12-20 21:22:14 +0530

    cors and build updated

--- craxinno-server/index.js
+++ craxinno-server/index.js
@@ -14,7 +14,7 @@ app.use(morgan("dev"));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(
     cors({
-        origin: ["http://localhost:3000", "http://localhost:5173"],
+        origin: ["http://localhost:3000", "http://localhost:5173", "https://craxxino-client.vercel.app"],
     })
 );
 app.use(express.static("public"));
