bdce63508ed293ef08a768331d085522027ddc2d
unais-k <unaissafa008@gmail.com>
2023-12-20 21:23:01 +0530

    cors

--- craxinno-client/src/services/service.ts
+++ craxinno-client/src/services/service.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 export const handleCreate = async (data: Object) => {
-    let response = await axios.post("https://craxxino-server-705scgno1-unais-k.vercel.app/create-user", data);
+    let response = await axios.post("https://craxxino-server-705scgno1-unais-k.vercel.app/create-user/", data);
 
     const responseData = response.data;
     if (!responseData.status) {
