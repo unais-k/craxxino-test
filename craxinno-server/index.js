c25a3c7152bf8074ba512c966eb40c517a85efd7
unais-k <unaissafa008@gmail.com>
2023-12-21 07:19:07 +0530

    api route changed

--- craxinno-server/index.js
+++ craxinno-server/index.js
@@ -10,6 +10,7 @@ import clientRoute from "./Router/route.js";
 
 const app = express();
 app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.static("public"));
 app.use(morgan("dev"));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(
@@ -17,12 +18,15 @@ app.use(
         origin: ["http://localhost:3000", "http://localhost:5173", "https://craxxino-client.vercel.app"],
     })
 );
-app.use(express.static("public"));
 app.use(cookieParser());
+
+app.get("/", (req, res) => {
+    console.log("connection route");
+});
 
 app.use("/", clientRoute);
 
-const PORT = 4001;
+const PORT = 8002;
 mongoose
     .connect(process.env.MONGODB_URL)
     .then(() => {
